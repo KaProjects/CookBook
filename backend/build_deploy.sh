c18ac99f5f6c393a4d68a567e1fb3cbf5eca3348
Stanislav Kaleta <kstanleykale@gmail.com>
2022-02-01 02:51:31 +0100

    fixes

--- backend/build_deploy.sh
+++ backend/build_deploy.sh
@@ -1,7 +1,7 @@
 # decrypt or create app.props
 
 # TODO fix tests
-mvn clean package assembly:single -DskipTests=True
+mvn clean install -DskipTests=True
 
 docker build -t cookbook-backend:v1.0 .
 docker run -d cookbook-backend:v1.0
